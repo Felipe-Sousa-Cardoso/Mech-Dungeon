1a2d78786c9e96a5dd2a1f94b29a791b1d02e832
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-04-08 20:49:44 -0300

    Correção de bugs

--- Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Choque/ChoqueInstanciado.cs
+++ Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Choque/ChoqueInstanciado.cs
@@ -20,23 +20,26 @@ public class ChoqueInstanciado : MonoBehaviour
     }//Metodo de acesso usado para registrar o primeiro alvo da colisão
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Inimigo")
-        {       
-            if(collision.transform!=alvo1&&alvo2==null) //escolhe o primeiro alvo como inimigo
+        if (alvo1 != null)
+        {
+            if (collision.gameObject.tag == "Inimigo")
             {
-                alvo2 = collision.transform;
-                choque();
-            }
-            if (collision.transform != alvo1 && //continua verificando se a colisão não é o primeiro alvo
-                Vector2.Distance(alvo1.position,alvo2.position) //calcula a distancia entre o primeiro alvo e o alvo atual
-                >Vector2.Distance(alvo1.position,collision.transform.position)) //se essa distancia for maior que a nova colisão substitue o alvo atual,
-                                                                                //focando assim o mais proximo
-            {
-                alvo2 = collision.transform;
-                choque();
+                if (collision.transform != alvo1 && alvo2 == null) //escolhe o primeiro alvo como inimigo
+                {
+                    alvo2 = collision.transform;
+                    choque();
+                }
+                if (collision.transform != alvo1 && //continua verificando se a colisão não é o primeiro alvo
+                    Vector2.Distance(alvo1.position, alvo2.position) //calcula a distancia entre o primeiro alvo e o alvo atual
+                    > Vector2.Distance(alvo1.position, collision.transform.position//se essa distancia for maior que a nova colisão substitue o alvo atual,
+                                                                                   //focando assim o mais proximo
+                    ))
+                {
+                    alvo2 = collision.transform;
+                    choque();
 
+                }
             }
-            
         }
     } //colisão e detecção do alvo mais proximo
     private void Update()
@@ -59,7 +62,10 @@ public class ChoqueInstanciado : MonoBehaviour
 
     void choque()
     {
-        ln.SetPosition(0, alvo1.position);
-        ln.SetPosition(1, alvo2.position);
+        if (alvo1 != null & alvo2 != null)
+        {
+            ln.SetPosition(0, alvo1.position);
+            ln.SetPosition(1, alvo2.position);
+        }      
     }
 }
