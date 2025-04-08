5495c50477fe74af17ba5e1a7d0409975d6fffea
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-04-08 19:21:20 -0300

    Melhoria na detecção de alvos do choque instanciado

--- Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Choque/ChoqueInstanciado.cs
+++ Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Choque/ChoqueInstanciado.cs
@@ -14,7 +14,14 @@ public class ChoqueInstanciado : MonoBehaviour
     {
         if (collision.gameObject.tag == "Inimigo")
         {       
-            if(collision.transform!=alvo1&&alvo2==null) //escolhe um unico inimigo como alvo
+            if(collision.transform!=alvo1&&alvo2==null) //escolhe o primeiro alvo como inimigo
+            {
+                alvo2 = collision.transform;
+            }
+            if (collision.transform != alvo1 && //continua verificando se a colisão não é o primeiro alvo
+                Vector2.Distance(alvo1.position,alvo2.position) //calcula a distancia entre o primeiro alvo e o alvo atual
+                >Vector2.Distance(alvo1.position,collision.transform.position)) //se essa distancia for maior que a nova colisão substitue o alvo atual,
+                                                                                //focando assim o mais proximo
             {
                 alvo2 = collision.transform;
             }
