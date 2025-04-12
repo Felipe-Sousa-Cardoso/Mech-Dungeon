c6b558631d8e832dac103e26f246afd7f45f8b89
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-04-11 23:47:28 -0300

    Comentarios do sistema de acido e correção de bugs

--- Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Acido/AcidoInstanciado.cs
+++ Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Acido/AcidoInstanciado.cs
@@ -5,7 +5,7 @@ public class AcidoInstanciado : MonoBehaviour
 {
     public ParticleSystem ps;
     [SerializeField] Transform alvo;
-    float dano;
+    [SerializeField] float dano;
     float timer;
     int stacks;
     
@@ -19,49 +19,48 @@ public class AcidoInstanciado : MonoBehaviour
     {
         get { return dano; }
         set { dano = value; }
-    }
+    }//Adiciona o dano causado pelos disparos
     public int Stacks
     {
         get { return stacks; }
         set { stacks = value; }
-    }
+    }//Verifica se o dano pode continuar escalando
 
     private void FixedUpdate()
-    {
-        
-        if (stacks > 0&&alvo!=null)
+    {      
+        if (stacks > 0&&alvo!=null) //Verifica se existe algum stack para aplicar o ácido e o alvo ainda não morreu
         {          
             timer += Time.deltaTime;
-            if (timer >= 1f)
+            if (timer >= 1f) //Ativa o veneno a cada 1 segundo
             {
-                GetComponentInParent<IDanificavel>().Danificar(dano / 2);
+                IDanificavel obj = GetComponentInParent<IDanificavel>(); //Acessa o componente que implemente a interface IDanificavel no objeto pai
+                if (obj != null)
+                {
+                    GetComponentInParent<IDanificavel>().Danificar(dano / 2); //Causa a metade do dano que já foi causado do veneno
+                    dano = stacks-1* dano / stacks; //reduz um pouco o dano total
+                }              
                 timer = 0;
                 stacks--;                             
             }
         }
         Particula(Stacks * 4);
-        if (alvo == null)
+
+        if (alvo == null) //Quando o objeto pai é destruido desliga a emissão e espera 1 segundo para que todas as particulas desapareçam para destruir o objeto
         {
             Particula(0);
             Destroy(gameObject, 1f);
             
         }
     }
-
-    public void Particula(int sim)
+    public void Particula(int sim) //Atualiza o sistema de particulas
     {
-        var emissao = ps.emission;
+        var emissao = ps.emission; 
         emissao.rateOverTime = sim;
        
-    }
-
-
+    }  
     private void Start()
     {
         ps = GetComponent<ParticleSystem>();
-        transform.localPosition = new Vector3(0, 0, 0);
+        transform.localPosition = new Vector3(0, 0, 0); //centraliza o objeto instanciado na posição do objeto pai
     }
-
-
-
 }
--- Assets/Scripts/Inimigos/BaseInimigos.cs
+++ Assets/Scripts/Inimigos/BaseInimigos.cs
@@ -12,12 +12,14 @@ public class BaseInimigos : MonoBehaviour, IDanificavel
         if (vida <= 0)
         {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.simulated = false;
-            if (GetComponentInChildren<AcidoInstanciado>() != null)
+            rb.simulated = false; //Desliga a simulação de fisica quando a vida chega a 0, e está finalizando a destruição do objeto
+
+            if (GetComponentInChildren<AcidoInstanciado>() != null) //se o alvo possui um componente de ácido faz com que ele não seja mais
+                                                                    //filho desse objeto, evitando assim sua destruição quando esse objeto for destruido
             {
                 GetComponentInChildren<AcidoInstanciado>().transform.parent = null;
             }
-            Destroy(gameObject,0.3f);
+            Destroy(gameObject,0.3f); //Demora 0.3 segundos para destruir o objeto para concluir os efeitos
         }
     }   
 }
