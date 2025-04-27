e4a7a190d93a7ba497225a88b811cb6c0982cccf
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-04-26 23:30:05 -0300

    continuação escudo

--- Assets/Scripts/Equipamentos/Habilidades/Escudos/Escudos.cs
+++ Assets/Scripts/Equipamentos/Habilidades/Escudos/Escudos.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Escudos : CadaHabilidade
 {
+    [SerializeField] Transform Jogador;
     public override void UsarHabilidade(JogadorHabilidades jog)
     {
         print("Você usou um escudo chamado " + nome);
+        Jogador = jog.gameObject.transform;
     }
 }
