b8d09d8bfae34f2edabc933286381893af66a854
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-06-04 19:21:52 -0300

    Correção de bug

--- Assets/Scripts/InterfaceDeUsuario/MineMapa/AtivaDesativa.cs
+++ Assets/Scripts/InterfaceDeUsuario/MineMapa/AtivaDesativa.cs
@@ -5,10 +5,12 @@ public class AtivaDesativa : MonoBehaviour
 {
     [SerializeField] GameObject mineMapa;
     [SerializeField] GameObject marcador;
+    [SerializeField] GameObject recomeçar;
     private void Start()
     {
         mineMapa = GetComponentInChildren<ControladorDeMinimapa>().gameObject;
         marcador = GetComponentInChildren<Marcador>().gameObject;
+        recomeçar = GetComponentInChildren<Recomeçar>().gameObject;
     }
     private void Update()
     {
@@ -16,11 +18,13 @@ public class AtivaDesativa : MonoBehaviour
         {
             mineMapa.SetActive(true);
             marcador.SetActive(true);
+            recomeçar.SetActive(true);
         }
         else
         {
             mineMapa.SetActive(false);
             marcador.SetActive(false);
+            recomeçar.SetActive(false);
         }
     }
 
