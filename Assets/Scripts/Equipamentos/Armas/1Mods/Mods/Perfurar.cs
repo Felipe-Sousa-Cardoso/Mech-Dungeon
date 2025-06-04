fa851d8665f70e8bbfb10f3f24cf4ec086c2a1f6
Felipe-Sousa-Cardoso <expraka@gmail.com>
2025-06-04 19:16:54 -0300

    Correção de bug

--- Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Perfurar.cs
+++ Assets/Scripts/Equipamentos/Armas/1Mods/Mods/Perfurar.cs
@@ -17,11 +17,6 @@ public class Perfurar : CadaMod
         {
             GetComponent<Munição>().AoDano(collision);               
         }
-        if (collision.gameObject.CompareTag("Paredes"))
-        {
-            Destroy(gameObject);
-        }
-
     }
     
    
